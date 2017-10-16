07ead8226c02cf2bc264e29c829b5ab2a29d5d03
Peter Mooney <peter.mooney90@gmail.com>
2017-10-15 20:05:36 -0400

    Refactor MainActivity to QuestionActivity

--- app/src/main/java/com/bignerdranch/android/project3/QuestionActivity.java
+++ app/src/main/java/com/bignerdranch/android/project3/QuestionActivity.java
@@ -3,11 +3,11 @@ package com.bignerdranch.android.project3;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends FragmentActivity {
+public class QuestionActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_question);
     }
 }
